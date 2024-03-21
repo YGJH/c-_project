6dffdbe9bdf890cc90fb836725c7f1224e84071b
YGJH <jihuty@gmail.com>
2024-03-22 03:07:52 +0800

    wefoiw

--- gen_testcases.cpp
+++ gen_testcases.cpp
@@ -1,27 +1,25 @@
 #include<bits/stdc++.h>
 #include<random>
+#define pii pair<int,int>
+#define pb push_back
 using namespace std;
 signed main() {
     mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
-    uniform_int_distribution<> gen(-90 ,90);
-    int n = 4;
-    cout << n << endl;
-    for(int i = 0 ; i < n ; i++) {
-        for(int j = 0 ; j < 3; j++ ) {
-            cout << gen(mt) << ' ' ;
+    const int n = 5 , m = 6;
+    uniform_int_distribution<> gen(1 , n );
+    map<pair<int,int>,bool> mp;
+    cout << n << ' ' << m << endl;
+    for(int i = 0 ; i < m ; i++) {
+        int tmp1 = gen(mt) , tmp2 = gen(mt);
+        while( mp[{tmp1,tmp2}] ||tmp1 == tmp2) {
+            tmp1 = gen(mt);
+            tmp2 = gen(mt);
         }
-        cout << endl;
+        mp[{tmp1 , tmp2}]=1;
+        mp[{tmp2 , tmp1}]=1;
+
+        cout << tmp1 << ' ' << tmp2 << endl;
     }
-    // cout << endl;
-    // cout << 40 << endl;
-    // for(int i = 0 ; i < 40 ; i++) {
-    //     for(int j = 0 ; j < 3; j++ ) {
-    //         cout << gen(mt) << ' ' ;
-    //     }
-    //     cout << endl;
-    // }
-    // // cout << endl;
-    cout << 0 << endl;
     
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -112,7 +112,6 @@ using ll = long long;
 #define mmax(a,b) (a > b)?a:b
 #define mmin(a,b) (a<b)?a:b
 using namespace std;
-const int N=1e5+5;
 #define LOCAL
 #ifdef LOCAL    // =========== Local ===========
 void dbg() { cerr << '\n'; }
@@ -204,24 +203,6 @@ inline int poww(int a , int b) {
     }
     return ret % MOD;
 }
-
-
-// namespace int128IO {
-// 	istream& operator>>(istream& is, __int128& i) {
-// 		string s; is>>s; i = 0;
-// 		auto c=s.begin(); c+=(s[0]=='-');
-// 		for(; c!=s.end(); ++c) i=i*10+(*c-'0');
-// 		if(s[0]=='-') i=-i;
-// 		return is;
-// 	}
-// 	ostream& operator<<(ostream& os, __int128 i) {
-// 		string s; bool neg=false; if(i<0) neg=true, i=-i;
-// 		while(i) s+=('0'+i%10), i/=10;
-// 		if(neg) os<<'-';
-// 		for(auto c=s.rbegin();c!=s.rend();++c) os<<*c;
-// 		return os;
-// 	}
-// }
 
 template<class io>
 inline void re(io &x) {
