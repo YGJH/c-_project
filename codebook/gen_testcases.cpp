2a1de59b6dc49318e5c0349cf9ab9459f022d1b9
YGJH <jihuty@gmail.com>
2024-04-17 01:11:11 +0800

    jrifji

--- codebook/gen_testcases.cpp
+++ codebook/gen_testcases.cpp
@@ -6,8 +6,17 @@ using namespace std;
 signed main() {
    
   mt19937 now(chrono::steady_clock::now().time_since_epoch().count());
-  uniform_int_distribution<> gen(1, 10);
-  
+  uniform_int_distribution<> gen(0  ,1 );
+  string s;
+  s.pb(1);
+  for(int i = 0 ; i < 999 ; i++) {
+    if(gen(now)) 
+      s.pb('1');
+    else
+      s.pb('0');
+  }
+  cout << s << endl;
+  cout << 1000 << endl;
   return 0;
 }
 
