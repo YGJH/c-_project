4364ac0169fcacf4790e998539213e968c660dfc
YGJH <jihuty@gmail.com>
2024-05-15 21:01:46 +0800

    some

--- problems/small2large.cpp
+++ problems/small2large.cpp
@@ -6,63 +6,39 @@ using namespace std;
 #define all(x) (x).begin(),(x).end()
 int n , q;
 const int N = 2e5 + 11;
-int arr[N];
-int pa[N];
-int sz[N];
-int i , tmp1 , tmp2;
-vector<set<int>> contain(N);
-int tar[N];
-int findd(int a) {
-    return (pa[a] == a) ? a : pa[a] = findd(pa[a]);
-}
-void merge(int a , int b) {
-    a = findd(a) , b = findd(b);
-    // a = pa[a] , b = pa[b];
-    cerr << "a: " << a << " b: " << b << endl;
-    if(sz[a] > sz[b]) {
-        pa[b] = a;
-        swap(a , b);
-    }
-    for(const auto &p : contain[a]) {
-        contain[b].insert(p);
-    }
-    tar[a] = 0;
-    sz[tar[b]] = contain[tar[b]].size();
-    sz[tar[a]] = 0;
-    contain[tar[a]].clear();
-    for(const auto &p : contain[tar[b]]) {
-        cerr << p << ' ';
-    }
-    cerr << endl;
-    cout << sz[tar[b]] << endl;
-}
-void solve() {
-
-    for( i = 1 ; i <= n ; i++) {
-        cin >> tmp1;
-        contain[i].insert(tmp1);
-    }
-    for( i = 0 ; i < q ; i++) {
-        cin >> tmp1 >> tmp2;
-        merge(tmp1 , tmp2);
-        // cout << sz[findd(tmp2)] << endl;
-
-    }
-}
-
+int tmp , i , tmp2;
+vector<unordered_map<int,bool>> contain(N);
+int ans;
 signed main() {
-ios_base::sync_with_stdio(0);
-cin.tie(0);
     cin >> n >> q;
-    for(i = 0 ; i <= n + 1 ; i++) {
-        sz[i] = 1;
-        pa[i] = i;
-        tar[i] = i;
+    for(i = 1 ; i <= n ; i++) {
+        cin >> tmp;
+        (contain[i])[tmp] = 1;
     }
-    sz[0] = 0;
-    solve();
-
-
-return 0;
-
+    for(i = 1 ; i <= q ; i++) {
+        cin >> tmp >> tmp2;
+        ans = contain[tmp2].size();
+        if(contain[tmp].size() == 0) {
+            cout << ans << endl;
+            contain[tmp].clear();
+            continue;
+        }
+        else if(ans == 0) {
+            cout << contain[tmp].size() << endl;
+            for(const auto &p : contain[tmp]) {
+                (contain[tmp2])[p.first] = 1;
+            }
+            contain[tmp].clear();
+            continue;
+        }
+        for(const auto &p : contain[tmp]) {
+            if((contain[tmp2])[p.first]==0) {
+                ans++;
+                (contain[tmp2])[p.first] = 1;
+            }        
+        }
+        contain[tmp].clear();
+        cout << ans << endl;
+    }
+    return 0;
 }
