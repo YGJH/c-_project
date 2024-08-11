f8997bd89675b368838a7a7aa8ad94f07e2182af
YGJH <jihuty@gmail.com>
2024-08-11 23:51:37 +0800

    some

--- codebook/main.cpp
+++ codebook/main.cpp
@@ -157,6 +157,52 @@ template<typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag,tree_o
 //   return is;
 // }
 
+template<class T, T M> class modular {
+
+	public:
+	T value;
+	modular(T val=T()) {
+		value = val;
+		value %= M; while(value<0)value+=M; if(value>=M)value%=M;
+	}
+	template<class P> modular(P val) {
+		value = val;
+		value %= M; while(value<0)value+=M; if(value>=M)value%=M;
+	}
+	
+	T pow(T a, T b) {
+		T ret=1;
+		for(a%=M; b; b>>=1, a=a*a%M) if(b&1) ret=ret*a%M;
+		return ret;
+	}
+	modular operator+(modular m) {return modular(value+m.value);}
+	modular operator-(modular m) {return modular(value-m.value);}
+	modular operator*(modular m) {return modular(value*m.value);}
+	modular operator/(modular m) {return modular(value*pow(m.value, M-2));} // works if M is prime
+	modular operator+=(modular m) {*this=operator+(m); return *this;}
+	modular operator-=(modular m) {*this=operator-(m); return *this;}
+	modular operator*=(modular m) {*this=operator*(m); return *this;}
+	modular operator/=(modular m) {*this=operator/(m); return *this;}
+	modular operator++() {*this=operator+(1); return *this;}
+	modular operator++(int) {modular r=*this; operator++(); return r;}
+	modular operator--() {*this=operator-(1); return *this;}
+	modular operator--(int) {modular r=*this; operator--(); return r;}
+	template<class P> friend modular operator-(P v, modular m) {return modular(v-m.value);}
+	template<class P> friend modular operator/(P v, modular m) {return modular(v)/m;}
+	T get() {return value;}
+	//operator T() {return value;}
+
+	bool operator==(modular m) {return value==m.value;}
+	bool operator!=(modular m) {return value!=m.value;}
+	bool operator<(modular m) {return value<m.value;}
+	bool operator>(modular m) {return value>m.value;}
+	bool operator<=(modular m) {return value<=m.value;}
+	bool operator>=(modular m) {return value>=m.value;}
+
+	friend istream& operator>>(istream& is, modular& m) {is>>m.value; return is;}
+	friend ostream& operator<<(ostream& os, modular m) {os<<m.value; return os;}
+};
+
 template<typename T>
 ostream& operator<<(ostream& os, vector<T>& intermediate_array) { for (auto& a : intermediate_array) cout << a << ' '; return os; }
 template<typename T>
--- new_folder/test.cpp
+++ new_folder/test.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int32_t main() {
-    int a = 0 , b = 1;
-    cout << a << ' ' << b << endl;
-    for(int i = 1 ; i <= 11 ; i++) {
-        cout << a + b << endl;
-        int tmp = a ;
-        a = b;
-        b = tmp + b;
+using ll = long long;
+namespace m{
+    ll ret = 1;
+    ll pow(ll a, ll b) {
+        for(; b ; b >>= 1 , a *= a) {
+            if(b&1) {
+                ret *= a;
+            }
+        }
+        return ret;
     }
+}
+int32_t main() {
+    cout << (5e8 > m::pow(2, 21)) << endl;
 return 0;
 }
